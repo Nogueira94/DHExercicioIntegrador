89bada4966ed5c09906a374b12cdc04e44f8ebc8
Guilherme Nogueira <guilherme.nogueira11@fatec.sp.gov.br>
2020-09-23 21:30:59 -0300

    fix var

--- src/br/com/digitalhouse/exerciciointegrador/ArtefatoItem.kt
+++ src/br/com/digitalhouse/exerciciointegrador/ArtefatoItem.kt
@@ -1,7 +1,7 @@
 package br.com.digitalhouse.exerciciointegrador
 
 class ArtefatoItem(val artefato: Artefato,
-                   val qnt: Int) {
+                   var qnt: Int) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is ArtefatoItem) return false
