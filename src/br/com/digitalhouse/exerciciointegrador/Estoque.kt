d99282eee5e269a78c7ce1837632cbab505a8211
Luiz Yamaoka <luiz.yamaoka@gmail.com>
2020-09-23 21:22:22 -0300

    criado estoque

---
+++ src/br/com/digitalhouse/exerciciointegrador/Estoque.kt
@@ -0,0 +1,38 @@
+package br.com.digitalhouse.exerciciointegrador
+
+import br.com.digitalhouse.exerciciointegrador.exceptions.SemEstoqueException
+import br.com.digitalhouse.exerciciointegrador.exceptions.ProdutoNaoEncontradoException
+
+class Estoque {
+
+    val itens = mutableListOf<ArtefatoItem>()
+
+    fun consultar(codigo: Int) : ArtefatoItem {
+        itens.forEach { if (it.artefato.codigo == codigo) return it }
+        throw ProdutoNaoEncontradoException()
+    }
+
+    fun adicionar(item: ArtefatoItem) {
+        itens.forEach {
+            if (it.artefato == item.artefato) {
+                it.qnt += item.qnt
+                return
+            }
+        }
+        itens.add(item)
+    }
+
+    fun remover(item: ArtefatoItem) {
+        itens.forEach {
+            if (it.artefato == item.artefato) {
+                if (it.qnt >= item.qnt) {
+                    it.qnt -= item.qnt
+                    return
+                } else
+                    throw SemEstoqueException()
+            }
+        }
+        throw ProdutoNaoEncontradoException()
+    }
+
+}
---
+++ src/br/com/digitalhouse/exerciciointegrador/exceptions/SemEstoqueException.kt
@@ -0,0 +1,3 @@
+package br.com.digitalhouse.exerciciointegrador.exceptions
+
+class SemEstoqueException() : Exception()
